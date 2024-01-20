a16e96ac756efddccef986e2a3316a744496c824
xlaoru <sia212007@gmail.com>
2024-01-20 15:29:09 +0200

    v2.0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
       <ul>
-        <li>Hello! It's an Initial App</li>
+        <li>Hello! It's an Initial App.</li>
+        <li>Hello! It's second dev! I'm creating new message to the first dev.</li>
       </ul>
   );
 }

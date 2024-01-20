2c34fa91258b0fadcc198ecc0b78bc147df4aa3f
xlaoru <sia212007@gmail.com>
2024-01-20 15:45:56 +0200

    v4.0

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
         <li>Hello! It's an Initial App.</li>
         <li>Hello! It's second dev! I'm creating new message to the first dev.</li>
         <li>Hello! It's first dev! I recieved your msg. Thank u! See u later.</li>
+        <li>Hello! It's second dev! I recieved your msg. It's end. Testing is up.</li>
       </ul>
   );
 }
